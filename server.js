6b628d7e7c8fa2844710559e4c84d68432500583
duhanpower <duhanpower@gmail.com>
2015-04-12 17:28:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var origin = 'tricksduniya.com:80';	port = process.env.OPENSHIFT_NODEJS_PORT,
+var	port = process.env.OPENSHIFT_NODEJS_PORT,
 	ip = process.env.OPENSHIFT_NODEJS_IP;
 
 var http = require('http'),
